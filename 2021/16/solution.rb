75da2ca239be2c4d7701caf7a0c4840d5dd060cc
Paul Scheid <pscheid@flexport.com>
2022-01-01 14:31:01 -0500

    day 16

--- 2021/16/solution.rb
+++ 2021/16/solution.rb
@@ -10,25 +10,27 @@ def parse(line)
 end
 
 def parse_literal(string, ptr)
-  group_count = 0
+  value = ""
   while string[ptr] == "1"
+    value += string[ptr+1...ptr+5].join
     ptr +=5
-    group_count += 1
-    # don't do anything with this for now
   end
 
+  value += string[ptr+1...ptr+5].join
   ptr += 5 # one more
+  # binding.pry
+  [ptr, value.to_i(2)]
 end
 
 def parse_operator(string, ptr)
   # Parsing the common header
-  value = string[ptr...ptr+3].join.to_i(2)
   ptr += 3
   type_id = string[ptr...ptr+3].join.to_i(2)
   ptr += 3
 
   if type_id == 4
-    ptr = parse_literal(string, ptr)
+    ptr, value = parse_literal(string, ptr)
+    return [ptr, value]
   else
     length_type_id = string[ptr]
     ptr += 1
@@ -36,28 +38,55 @@ def parse_operator(string, ptr)
       length_in_bits = string[ptr...ptr+15].join.to_i(2)
       ptr += 15
       orig_ptr = ptr
+      values = []
       while orig_ptr + length_in_bits > ptr
         ptr, result_value = parse_operator(string, ptr)
-        value += result_value
+        values << result_value
       end
+
     else
       num_sub_packets = string[ptr...ptr+11].join.to_i(2)
       ptr += 11
+      values = []
       num_sub_packets.times do
         ptr, result_value = parse_operator(string, ptr)
-        value += result_value
+        values << result_value
       end
     end
   end
+  value = values_to_value(values, type_id)
   [ptr, value]
 end
 
-lines = File.open("test.txt").readlines()
-# parse(lines.first)
+def values_to_value(values, type_id)
+  case type_id
+  when 0
+    values.sum
+  when 1
+    values.inject(:*)
+  when 2
+    values.min
+  when 3
+    values.max
+  when 5
+    values.first > values.last ? 1 : 0
+  when 6
+    values.first < values.last ? 1 : 0
+  when 7
+    values.first == values.last ? 1 : 0
+  else
+    raise "invalid type_id"
+  end
+end
 
-parse(lines[0])
-parse(lines[1])
-parse(lines[2])
-parse(lines[3])
-parse(lines[4])
-parse(lines[5])
+lines = File.open("input.txt").readlines()
+parse(lines.first)
+
+# parse(lines[0])
+# parse(lines[1])
+# parse(lines[2])
+# parse(lines[3])
+# parse(lines[4])
+# parse(lines[5])
+# parse(lines[6])
+# parse(lines[7])
